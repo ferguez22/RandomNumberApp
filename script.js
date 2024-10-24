25111c8a8875a96dfdd8472d2f2bb8a950d4c4f7
Fernando <98433286+ferguez22@users.noreply.github.com>
2024-10-24 13:52:30 +0200

    Añadiendo TryCatch

--- script.js
+++ script.js
@@ -12,83 +12,119 @@ let numerosGenerados = []; // Array para almacenar los números generados
 generar.addEventListener("click", generarNumeroAleatorio);
 
 function generarNumeroAleatorio() {
-    console.log("click");
-    if (!isGenerating) {
-        isGenerating = true;
-        reproducirAudioDrums();
-        setTimeout(() => {
-            cambiarEstadoBoton();
-            limpiarResultado();
-
-            // Mostrar la caja
-            caja.style.display = "flex"; // Cambiar a visible
-
-            // Generar número cada 70 milisegundos
-            const interval = setInterval(() => {
-                const numero = Math.floor(Math.random() * 300);
-                mostrarResultado(numero);
-                ultimoNumero = numero; // Guardar el último número generado
-            }, 70);
-
-            // Detener el intervalo después de 6 segundos, mostrar el resultado final y reproducir audio de éxito
+    try {
+        console.log("click");
+        if (!isGenerating) {
+            isGenerating = true;
+            reproducirAudioDrums();
             setTimeout(() => {
-                clearInterval(interval);
-                reproducirAudioSuccess();
-                mostrarUltimoNumero(); // Mostrar el último número generado
-                restaurarEstado(); // Restaurar el estado al final
-                isGenerating = false; // Permitir nuevos clics
-                confetti({
-                    particleCount: 3000, // Aumentar la cantidad de confetti
-                    spread: 900, // Ajustar la dispersión
-                    origin: { y: 0.6 }, // Ajustar la posición de origen
-                    colors: ['#ff0', '#0f0', '#00f', '#f00'], // Colores del confetti
-                });
-            }, 3000); // Cambiar a 6000 para que coincida con el tiempo total
-        }, 900); // Esperar 1 segundo antes de generar números
+                cambiarEstadoBoton();
+                limpiarResultado();
+
+                // Mostrar la caja
+                caja.style.display = "flex"; // Cambiar a visible
+
+                // Generar número cada 70 milisegundos
+                const interval = setInterval(() => {
+                    const numero = Math.floor(Math.random() * 300);
+                    mostrarResultado(numero);
+                    ultimoNumero = numero; // Guardar el último número generado
+                }, 70);
+
+                // Detener el intervalo después de 6 segundos, mostrar el resultado final y reproducir audio de éxito
+                setTimeout(() => {
+                    clearInterval(interval);
+                    reproducirAudioSuccess();
+                    mostrarUltimoNumero(); // Mostrar el último número generado
+                    restaurarEstado(); // Restaurar el estado al final
+                    isGenerating = false; // Permitir nuevos clics
+                    confetti({
+                        particleCount: 3000, // Aumentar la cantidad de confetti
+                        spread: 900, // Ajustar la dispersión
+                        origin: { y: 0.6 }, // Ajustar la posición de origen
+                        colors: ['#ff0', '#0f0', '#00f', '#f00'], // Colores del confetti
+                    });
+                }, 3000); // Cambiar a 6000 para que coincida con el tiempo total
+            }, 900); // Esperar 1 segundo antes de generar números
+        }
+    } catch (error) {
+        console.error("Error al generar número aleatorio: ", error);
     }
 }
 
 function mostrarUltimoNumero() {
-    resultado.textContent = ultimoNumero; // Mostrar el último número generado
-    resultado.style.fontSize = "6rem"; // Mantener el tamaño grande del texto
-    numerosGenerados.push(ultimoNumero); // Agregar el número al array
-    mostrarHistorial(); // Mostrar el historial actualizado
+    try {
+        resultado.textContent = ultimoNumero; // Mostrar el último número generado
+        resultado.style.fontSize = "6rem"; // Mantener el tamaño grande del texto
+        numerosGenerados.push(ultimoNumero); // Agregar el número al array
+        mostrarHistorial(); // Mostrar el historial actualizado
+    } catch (error) {
+        console.error("Error al mostrar el último número: ", error);
+    }
 }
 
 function mostrarHistorial() {
-    historial.innerHTML = ""
-    numerosGenerados.forEach(numero => {
-        const p = document.createElement("p");
-        p.textContent = numero; // Mostrar cada número en el historial
-        historial.appendChild(p);
-    });
+    try {
+        historial.innerHTML = ""
+        numerosGenerados.forEach(numero => {
+            const p = document.createElement("p");
+            p.textContent = numero; // Mostrar cada número en el historial
+            historial.appendChild(p);
+        });
+    } catch (error) {
+        console.error("Error al mostrar el historial: ", error);
+    }
 }
 
 function mostrarResultado(numero) {
-    resultado.textContent = numero; // Mostrar el número actual
-    resultado.classList.add("resultado-generado");
-    resultado.style.fontSize = "8rem"; // Ajustar el tamaño del texto
+    try {
+        resultado.textContent = numero; // Mostrar el número actual
+        resultado.classList.add("resultado-generado");
+        resultado.style.fontSize = "8rem"; // Ajustar el tamaño del texto
+    } catch (error) {
+        console.error("Error al mostrar el resultado: ", error);
+    }
 }
 
 function cambiarEstadoBoton() {
-    generar.classList.add("pressed");
+    try {
+        generar.classList.add("pressed");
+    } catch (error) {
+        console.error("Error al cambiar el estado del botón: ", error);
+    }
 }
 
 function limpiarResultado() {
-    resultado.textContent = ""; // Limpiar el resultado antes de mostrar nuevos números
+    try {
+        resultado.textContent = ""; // Limpiar el resultado antes de mostrar nuevos números
+    } catch (error) {
+        console.error("Error al limpiar el resultado: ", error);
+    }
 }
 
 function reproducirAudioDrums() {
-    const audio = new Audio("./audios/drums.mp3");
-    audio.play();
+    try {
+        const audio = new Audio("./audios/drums.mp3");
+        audio.play();
+    } catch (error) {
+        console.error("Error al reproducir el audio de tambores: ", error);
+    }
 }
 
 function reproducirAudioSuccess() {
-    new Audio("./audios/success.mp3").play();
+    try {
+        new Audio("./audios/success.mp3").play();
+    } catch (error) {
+        console.error("Error al reproducir el audio de éxito: ", error);
+    }
 }
 
 function restaurarEstado() {
-    generar.classList.remove("pressed");
-    resultado.classList.remove("resultado-generada");
-    caja.classList.remove("expandida");
+    try {
+        generar.classList.remove("pressed");
+        resultado.classList.remove("resultado-generada");
+        caja.classList.remove("expandida");
+    } catch (error) {
+        console.error("Error al restaurar el estado: ", error);
+    }
 }
